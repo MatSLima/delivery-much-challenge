963f5220ceb72b2cee86beb4c91b5e82028c1616
Matheus Lima <matsdelima@gmail.com>
2020-10-22 00:51:06 -0300

    fix(tests): adjust path to fixtures on recipe tests

--- src/use-cases/list-recipes.spec.js
+++ src/use-cases/list-recipes.spec.js
@@ -1,7 +1,9 @@
-import {makeFakeGiphyRepository, makeFakeRecipesRepository} from '../../../__test__/fixtures/repository'
-import {makeRecipe1, makeGiphyResponse1} from '../../../__test__/fixtures/recipe'
-import makeRecipe from '../../entity/recipes'
+import {makeFakeGiphyRepository, makeFakeRecipesRepository} from '../../__test__/fixtures/repository'
+import {makeRecipe1, makeGiphyResponse1} from '../../__test__/fixtures/recipe'
+import buildMakeRecipe from '../entity/recipe'
 import makeListRecipes from './list-recipes'
+
+const makeRecipe = buildMakeRecipe({})
 
 describe('get recipes', () => {
   it('should return response body', async () => {
