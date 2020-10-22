3404ccd801143b626a0b70ce5a769ca9966e87f5
Matheus Lima <matsdelima@gmail.com>
2020-10-22 00:56:59 -0300

    fix(controllers): adjust recipe use case path

--- src/controllers/recipes/index.js
+++ src/controllers/recipes/index.js
@@ -1,13 +1,15 @@
+import {
+  recipeService,
+} from '../../use-cases/recipes'
 import makeGetRecipes from './get-recipes'
-  
-  
-  const getRecipes = makeGetRecipes({
-    listRecipes: () => {return []}
-  })
-  
-  const recipeController = Object.freeze({
-    getRecipes
-  })
-  
-  export { recipeController }
-  
+
+
+const getRecipes = makeGetRecipes({
+  listRecipes: recipeService.listRecipes
+})
+
+const recipeController = Object.freeze({
+  getRecipes
+})
+
+export { recipeController }
